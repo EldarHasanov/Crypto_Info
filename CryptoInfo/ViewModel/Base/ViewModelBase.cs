55fab43b69b32f72bc392e2bfe54665fcab99f1b
EldarHasanov <eldargasanovtr91@gmail.com>
2022-05-14 17:00:33 +0300

    Added interface IDisposable to ViewModelBaseClass

--- CryptoInfo/ViewModel/Base/ViewModelBase.cs
+++ CryptoInfo/ViewModel/Base/ViewModelBase.cs
@@ -1,12 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CryptoInfo.ViewModel.Base
 {
-    internal class ViewModelBase
+    internal abstract class ViewModelBase : INotifyPropertyChanged, IDisposable
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if(Equals(field,value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+
+        ~ViewModelBase()
+        {
+            Dispose(false);
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        private bool _Disposed;
+        protected virtual void Dispose(bool Disposing)
+        {
+            if (!Disposing || _Disposed) return;
+            _Disposed = true;
+            //Release managed resources
+        }
     }
 }
