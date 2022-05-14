c53f5f6ee81be7f161cdbc1e8b98b0ef05f3b5cf
EldarHasanov <eldargasanovtr91@gmail.com>
2022-05-14 21:20:31 +0300

    Testing view model

--- CryptoInfo/ViewModel/MainWindowViewModel.cs
+++ CryptoInfo/ViewModel/MainWindowViewModel.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CryptoInfo.ViewModel.Base;
 
 namespace CryptoInfo.ViewModel
 {
-    internal class MainWindowViewModel
+    internal class MainWindowViewModel : ViewModelBase
     {
+        private string _Title = "Top 10 cryptocurrencies"; 
+
+        /// <summary>Title of window</summary>
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
     }
 }
