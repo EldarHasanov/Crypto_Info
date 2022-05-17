8fd96e83a6c1712decce942c786e136f15bb81bc
EldarHasanov <eldargasanovtr91@gmail.com>
2022-05-17 16:59:11 +0300

    Created cinsole app to test API

---
+++ CryptoInfo/Infrastructure/Commands/CloseApplicationCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using CryptoInfo.Infrastructure.Commands.Base;
+
+namespace CryptoInfo.Infrastructure.Commands
+{
+    internal class CloseApplicationCommand : BaseCommand
+    {
+        public override bool CanExecute(object parameter) => true;
+
+        public override void Execute(object parameter) => Application.Current.Shutdown();
+    }
+}
