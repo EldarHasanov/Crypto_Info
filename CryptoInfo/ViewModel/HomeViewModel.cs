06d1078435dbdd8f0206620d67b8069434a01091
EldarHasanov <eldargasanovtr91@gmail.com>
2022-05-19 03:10:43 +0300

    Relise home page

--- CryptoInfo/ViewModel/HomeViewModel.cs
+++ CryptoInfo/ViewModel/HomeViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using CryptoInfo.Models;
 
@@ -9,6 +11,7 @@ namespace CryptoInfo.ViewModel
 {
     internal class HomeViewModel
     {
+        private const string coincap_assets_url = @"http://api.coincap.io/v2/assets";
         public List<Cryptocurrency> TopTenCryptocurrencies { get; set; }
 
         public HomeViewModel(List<Cryptocurrency> topTenCryptocurrencies)
@@ -23,7 +26,28 @@ namespace CryptoInfo.ViewModel
 
         public HomeViewModel()
         {
-            TopTenCryptocurrencies = new List<Cryptocurrency>();
+            var client = new HttpClient();
+            if (client != null)
+            {
+                var responseMessage = client.GetAsync(coincap_assets_url).Result;
+                if (responseMessage.IsSuccessStatusCode)
+                {
+                    var jsonStringResult = responseMessage.Content.ReadAsStringAsync().Result;
+
+                    AssetsRootObjectWithList rootobject = JsonSerializer.Deserialize<AssetsRootObjectWithList>(jsonStringResult);
+
+                    TopTenCryptocurrencies = rootobject.data.Take(10).ToList();
+                    foreach (var element in TopTenCryptocurrencies)
+                    {
+                        element.priceUsd = element.priceUsd.Substring(0, 8) + " USD";
+                        element.changePercent24Hr = element.changePercent24Hr.Substring(0, 5) + " %";
+                    }
+                }
+                else
+                {
+                    throw new FieldAccessException("Something wrong with url");
+                }
+            }
         }
     }
 }
